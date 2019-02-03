848cfb45bc06092b2ef147626242c0a7a416efe9
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-02-02 19:22:37 -0500

    Porting the backend over (With filler server credentials)

---
+++ server.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'nun',
+        password : 'ya',
+        database : 'beeswax'
+    }
+})
+
+console.log(knex.select('*').from('users'));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const database = {
+    user: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.user);
+})
+
+app.post('/signin', (req, res) => {
+    knex
+        .select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.passwod, data[0].hash);
+            if (isValid) {
+                return(
+                    knex
+                        .select('*')
+                        .from('users')
+                        .where('email', '=', req.body.email)
+                        .then(user => {
+                            res.json(user[0])
+                            })
+                        .catch(err => res.status(400))
+            )}})
+        .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    knex
+        .transaction(trx => {
+        trx
+            .insert({
+                hash: hash,
+                email: email 
+                })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                            email: loginEmail[0],
+                            name: name,
+                            joined: new Date()
+                            })
+                        .then(user => {
+                            res.json(user[0]);
+                            })
+                 })
+                 .then(trx.commit)
+                 .catch(trx.rollback)
+            })
+        .catch(err => res.status(400))
+
+})
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    knex('users')
+        .select('*')
+        .from('users')
+        .where({id: id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('Not Found');
+            }
+        })
+    })
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    knex('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+            })
+        .catch(err => escape.status(400).json('unable to get entries'))
+})
+
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000');
+})
